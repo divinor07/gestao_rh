09ccc5764198fe9606c507940de0589bbb7b9f77
Divino Rodrigues <divino.sr@hotmail.com>
2020-04-27 19:13:40 -0300

    Revert "Implementando o crud de documentos"
    This reverts commit ea026c1a0dddadda402f2bf0d8293b7393f67b7f.

--- apps/funcionarios/urls.py
+++ apps/funcionarios/urls.py
@@ -8,6 +8,6 @@ from .views import \
 urlpatterns = [
     path('', FuncionariosList.as_view(), name='list_funcionarios'),
     path('novo/', FuncionarioNovo.as_view(), name='create_funcionario'),
-    path('editar/<int:pk>/', FuncionarioEdit.as_view(), name='update_funcionario'),
+    path('editar/<int:pk>/', FuncionarioEdit.as_view(), name='edit_funcionario'),
     path('delete/<int:pk>/', FuncionarioDelete.as_view(), name='delete_funcionario'),
 ]

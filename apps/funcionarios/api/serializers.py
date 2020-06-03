660f3e61221ed28ca4fd5ab4b6b0bb65e8f4f480
Divino Rodrigues <divino.sr@hotmail.com>
2020-06-02 22:22:46 -0300

    Criando o endpoint de banco de horas e mesclando com funcionarios

--- apps/funcionarios/api/serializers.py
+++ apps/funcionarios/api/serializers.py
@@ -1,8 +1,12 @@
 from rest_framework import serializers
 from apps.funcionarios.models import Funcionario
+from apps.registro_hora_extra.api.serializers import RegistroHoraExtraSerializer
 
 
 class FuncionarioSerializer(serializers.ModelSerializer):
+    registrohoraextra_set = RegistroHoraExtraSerializer(many=True)
+
     class Meta:
         model = Funcionario
-        fields = ['nome', 'departamentos', 'empresa', 'user']
+        fields = ['id', 'nome', 'departamentos', 'empresa', 'user',
+                  'total_horas_extra', 'registrohoraextra_set']
